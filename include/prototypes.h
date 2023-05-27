d7f9ac0f0ff5794c0b2016f9ce728709a64bb9be
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2023-05-27 20:40:26 +0100

    Adding the program skelleton.

---
+++ include/prototypes.h
@@ -0,0 +1,13 @@
+// system specific commands
+#ifdef _WIN32 // assume Windows
+    #define clear_screen() system("cls")
+#else // assume POSIX or similar
+    #define clear_screen() system("clear")
+#endif
+
+// menuManager headers
+void main_menu();
+void insert_any_key();
+void invalid_option();
+
+// fileManager headers
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "include\prototypes.h"
+
+int main()
+{
+    main_menu();
+    return 0;
+}
