c55ae16c6c2d24152bf3e4da12ea4f24e2726bb9
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2023-05-28 18:30:18 +0100

    Pequenas mudanças e adição do .gitignore

--- main.c
+++ main.c
@@ -1,8 +1,99 @@
-#include <stdio.h>
-#include "include\prototypes.h"
+// --- system specific commands ---
+#ifdef _WIN32 // assume Windows
+    #define clear_screen() system("cls")
+#else // assume POSIX or similar
+    #define clear_screen() system("clear")
+#endif
 
+// --- includes ---
+#include <stdio.h>
+#include <stdlib.h>
+
+// --- prototypes ---
+void main_menu();
+void insert_any_key();
+void invalid_option();
+void clear_buffer();
+
+// --- main function start---
 int main()
 {
     main_menu();
     return 0;
+}
+// --- main function end ---
+
+// --- auxiliary functions ---
+// --- --- menu texts --- ---
+void main_menu_text()
+{
+    clear_screen();
+    printf("[1] - Teste 1\n");
+    printf("[2] - Teste 2\n");
+    printf("[3] - Teste 3\n");
+    printf("[4] - Teste 4\n");
+    printf("[0] - Sair\n");
+    printf("\n>>> ");
+}
+
+// --- --- menu functions --- ---
+void main_menu()
+{
+    int option;;
+    do
+    {
+        main_menu_text();
+        scanf("%d", &option);
+        clear_buffer();
+        switch (option)
+        {
+        case 1:
+            clear_screen();
+            printf("Teste 1\n");
+            insert_any_key();
+            break;
+        case 2:
+            clear_screen();
+            printf("Teste 2\n");
+            insert_any_key();
+            break;
+        case 3:
+            clear_screen();
+            printf("Teste 3\n");
+            insert_any_key();
+            break;
+        case 4:
+            clear_screen();
+            printf("Teste 4\n");
+            insert_any_key();
+            break;
+        case 0:
+            clear_screen();
+            printf("Saindo...\n");
+            break;
+        default:
+            invalid_option();
+            break;
+        }
+    } while (option != 0);
+}
+
+// --- --- utility functions --- ---
+void clear_buffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void insert_any_key()
+{
+    printf("Pressione qualquer tecla para continuar...");
+    getchar();
+}
+
+void invalid_option()
+{
+    clear_screen();
+    printf("Opção inválida!\n");
+    insert_any_key();
 }
